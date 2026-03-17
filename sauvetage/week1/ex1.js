3718738be98f418d9f634e07553cda4c2c326540
AyoubKhaoua <mr.ayoubkhaoua@example.com>
2026-03-17 15:46:44 +0100

    ex1 done

--- sauvetage/week1/ex1.js
+++ sauvetage/week1/ex1.js
@@ -1,9 +1,9 @@
 /* Pensez a filter() pour eliminer les falsy et les doublons.
 Pour les doublons : indexOf() ou includ() peuvent vous aider. */
 let tableau = [false, 7, 7, "", 8, undefined, 8, 4];
-function nettoyer(tableau) {
+/* function nettoyer(tableau) {
   tableau.forEach((tb) => {
-    if (!tb || tb === 0 || tb === "" /* || tableau.includes(tb) */) {
+    if (!tb || tb === 0 || tb === "" || tableau.includes(tb)) {
       let index = tableau.indexOf(tb);
 
       tableau = tableau
@@ -12,5 +12,17 @@ function nettoyer(tableau) {
     }
   });
   console.log(tableau);
+} */
+function nettoyer(tableau) {
+  let resultat = [];
+
+  tableau.forEach((tb) => {
+    if (tb && !resultat.includes(tb)) {
+      resultat.push(tb);
+    }
+  });
+
+  resultat.sort((a, b) => a - b);
+  console.log(resultat);
 }
 nettoyer(tableau);
